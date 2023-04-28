5b97f4dd42fa932724d45e808ae1a4c000001402
akuchta <akuchta201@gmail.com>
2023-04-29 01:56:51 +0200

    pierwszy przeciwnik!
    chujowy troche to place hollder jak ccos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float movmentDistance;
+    [SerializeField] private float speed;
     [SerializeField] private float damage;
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
 
+    private void Awake () 
+    {
+        leftEdge = transform.position.x - movmentDistance;
+        rightEdge =transform.position.x + movmentDistance;
+    }
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.y);
+            }
+            else
+            {
+                movingLeft = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.y);
+            }
+            else
+            {
+                movingLeft = true;
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
