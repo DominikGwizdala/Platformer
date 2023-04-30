9fffb8fc73f363292d10849f01d5dd7bf1dda1a8
DominikGwizdala <93713131+DominikGwizdala@users.noreply.github.com>
2023-04-30 07:57:07 +0200

    yyy

---
+++ Assets/Scripts/Enemies/FireTrap.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("FireTrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator fireTrapAnimation;
+    private SpriteRenderer spriteRenderer;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        fireTrapAnimation = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFireTrap());
+            if (active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+    private IEnumerator ActivateFireTrap()
+    {
+        //uruchamianie pu³apki, zmiana koloru na czerwony dla czytelnoœci
+        triggered = true;
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(activationDelay);
+        //zmiana koloru na oryginalny i aktywacja pu³apki
+        spriteRenderer.color = Color.white;
+        active = true;
+        fireTrapAnimation.SetBool("active", true);
+        yield return new WaitForSeconds(activeTime);
+        //deaktywacja pu³aplki
+        active = false;
+        triggered = false;
+        fireTrapAnimation.SetBool("active", false);
+    }
+}
